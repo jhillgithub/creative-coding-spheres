7e465b95af4007ade23b61c335be39316127e4e3
jh <>
2023-12-01 19:09:28 -0600

    experimenting with bounces

--- src/components/GradientSpheres.tsx
+++ src/components/GradientSpheres.tsx
@@ -3,75 +3,115 @@ import {
   type InstancedMeshProps,
   type RootState,
 } from "@react-three/fiber";
-import { useMemo, useRef } from "react";
-import { InstancedMesh, Object3D, Vector3 } from "three";
+import { useMemo, useRef, type RefObject } from "react";
 import { rescale } from "src/utils";
+import {
+  BufferGeometry,
+  InstancedMesh,
+  Material,
+  Matrix4,
+  Object3D,
+  OrthographicCamera,
+  Vector3,
+  type NormalBufferAttributes,
+  MathUtils,
+} from "three";
 
-type Sphere = {
-  position: Vector3;
-  rotation?: Vector3;
-  scale?: Vector3;
-  direction?: Vector3;
+const getPosition = (index: number, mesh: InstancedMesh) => {
+  const matrix = new Matrix4();
+  mesh.getMatrixAt(index, matrix);
+  const position = new Vector3();
+  position.setFromMatrixPosition(matrix);
+  return position;
 };
+
+let delayit = 0;
+const updateDirection = (
+  index: number,
+  directions: Vector3[],
+  position: Vector3,
+  camera: OrthographicCamera,
+  radius: number
+) => {
+  if (
+    position.x <= camera.left + radius ||
+    position.x >= camera.right - radius
+  ) {
+    directions[index].x = -directions[index].x;
+  }
+  if (
+    position.y <= camera.bottom + radius ||
+    position.y >= camera.top - radius
+  ) {
+    directions[index].y = -directions[index].y;
+  }
+  const zLimit = Math.abs(camera.position.z) - radius - 2;
+  if (position.z <= -zLimit || position.z >= zLimit) {
+    directions[index].z = -directions[index].z;
+  }
+};
+
 interface GradientSpheresProps extends InstancedMeshProps {
   count: number;
 }
 export const GradientSpheres = ({ count, ...props }: GradientSpheresProps) => {
-  const sphere = useRef<InstancedMesh>(null);
-  /**Dummy variables used for positioning and movement */
-  const dummyTarget = new Object3D();
-  const spheres = useRef<Array<Sphere>>([]);
-
-  useMemo(() => {
-    // Initialize positions and movement vectors
-    for (let i = 0; i < count; i++) {
-      // Initial random position
-      const position = new Vector3(
-        Math.random() * 10 - 5,
-        Math.random() * 10 - 5,
-        Math.random() * 10 - 5
+  const meshRef = useRef<InstancedMesh>(null);
+  const directions = useMemo(() => {
+    return new Array(count)
+      .fill(null)
+      .map(() =>
+        new Vector3(
+          Math.random() * 2 - 1,
+          Math.random() * 2 - 1,
+          Math.random() * 2 - 1
+        ).normalize()
       );
-
-      // Initial random scale
-      const size = rescale(Math.random(), 0, 1, 0.25, 0.8);
-      const scale = new Vector3(size, size, size);
-
-      // Random movement vector
-      const direction = new Vector3(
-        Math.random() * 1.0 - 0.5,
-        Math.random() * 1.0 - 0.5,
-        Math.random() * 1.0 - 0.5
-      ).normalize();
-
-      spheres.current.push({ position, scale, direction });
-    }
   }, [count]);
+  const speed = 2;
+  const radius = 0.5;
 
   useFrame((state: RootState, delta: number) => {
-    if (!sphere.current) return;
+    if (!meshRef.current) return;
+    const camera = state.camera as OrthographicCamera;
 
     for (let i = 0; i < count; i++) {
-      const { position, scale, direction } = spheres.current[i];
-      // Update position based on movement vector and delta
-      if (direction) {
-        position.add(direction.clone().multiplyScalar(delta));
-      }
+      const position = getPosition(i, meshRef.current);
 
-      // Apply the updated position
-      dummyTarget.position.copy(position);
-      if (scale) {
-        dummyTarget.scale.copy(scale);
-      }
-      dummyTarget.updateMatrix();
-      sphere.current.setMatrixAt(i, dummyTarget.matrix);
+      /**Calculate new position */
+      position.x += directions[i].x * speed * delta;
+      position.y += directions[i].y * speed * delta;
+      position.z += directions[i].z * speed * delta;
+
+      /**Copy Position and set matrix*/
+      const dummy = new Object3D();
+      dummy.position.copy(position);
+      const zDist = camera.position.z - position.z;
+      const clamped = MathUtils.clamp(zDist, 0, position.z);
+      const rescaledZ = rescale(clamped, 0, 1, 0.2, 0.24);
+      // delayit++;
+      // if (delayit % 1000 === 0) {
+      //   // console.log(zDist, clamped);
+      // }
+      // // console.log(zDist, clamped);
+
+      dummy.scale.set(rescaledZ, rescaledZ, rescaledZ);
+      dummy.updateMatrix();
+      meshRef.current.setMatrixAt(i, dummy.matrix);
+
+      /**Invert direction if at camera bounds */
+      updateDirection(i, directions, position, camera, radius);
     }
 
-    sphere.current.instanceMatrix.needsUpdate = true;
+    meshRef.current.instanceMatrix.needsUpdate = true;
   });
 
   return (
-    <instancedMesh ref={sphere} {...props} args={[undefined, undefined, count]}>
-      <sphereGeometry args={[1, 32, 32]} />
+    <instancedMesh
+      ref={meshRef}
+      {...props}
+      args={[undefined, undefined, count]}
+    >
+      <sphereGeometry args={[radius, 32, 32]} />
       <meshStandardMaterial color="orange" />
     </instancedMesh>
   );
