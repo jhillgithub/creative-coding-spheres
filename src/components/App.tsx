8b5c3de338ea3075265f2e63336a8b0d9d786970
jh <>
2023-12-01 19:08:11 -0600

    adds suspense for scene

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,16 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { Scene } from "./Scene";
+import { Suspense } from "react";
 
 export const App = () => {
   return (
     <>
       <Canvas>
         <color attach="background" args={["slategray"]} />
-        <Scene />
+        <Suspense fallback={null}>
+          <Scene />
+        </Suspense>
         <OrbitControls />
       </Canvas>
     </>
